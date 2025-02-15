a3bd53b5d74fe9dc055bf2948aa27aa55fc429b0
Luigi-Faldetta <luigifaldetta@gmail.com>
2025-02-15 20:30:06 +0100

    feat: auth and consumer route set up

---
+++ src/app/(consumer)/layout.tsx
@@ -0,0 +1,43 @@
+import { SignedIn } from '@clerk/nextjs';
+import Link from 'next/link';
+import { ReactNode, Suspense } from 'react';
+
+export default function ConsumerLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
+
+function Navbar() {
+  return (
+    <header className="flex h-12 shadow bg-background z-10">
+      <nav className="flex gap-4 container">
+        <Link
+          className="mr-auto text-lg hover:underline px-2 flex items-center"
+          href="/"
+        >
+          LuigiCodes
+        </Link>
+        <Suspense>
+          <Link
+            className="hover:bg-accent/10 flex items-center px-2"
+            href="/courses"
+          >
+            My Courses
+          </Link>
+          <Link
+            className="hover:bg-accent/10 flex items-center px-2"
+            href="/purchases"
+          >
+            Purchase History
+          </Link>
+        </Suspense>
+      </nav>
+    </header>
+  );
+}
